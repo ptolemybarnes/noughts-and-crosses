601b33d5dc26c5e70b9085f91f89d682e2b9a401
Ptolemy <ptolemybarnes@gmail.com>
2014-11-17 22:32:09 -0500

    Add AI functionality: AI will now block player when player is about to win.

--- noughts-and-crosses.rb
+++ noughts-and-crosses.rb
@@ -79,37 +79,18 @@ class NoughtsAndCrosses
 	def get_user_move
 		puts "Please state your move:\n Enter a number or type 'exit' to quit."
 		move_input = gets.chomp
-			
-			case move_input
-				when "1"
-					move = 1
-				when "2"
-					move = 2
-				when "3"
-					move = 3
-				when "4"
-					move = 4
-				when "5"
-					move = 5
-				when "6"
-					move = 6
-				when "7"
-					move = 7
-				when "8"
-					move = 8
-				when "9"
-					move = 9
-				when "exit"
-					move = "exit"
-				else
-					puts "Invalid move"
-					move = false
-				end
-			if @move_log.include?(move)
-				puts "Move invalid: already made."
-				move = false
-			end
-		move
+		if move_input == "exit"
+			move = "exit"
+		elsif @move_log.include?(move_input.to_i)
+			puts "Move invalid: already made."
+			move = false
+		elsif move_input.to_i == 0
+			puts "Invalid move"
+			move = false
+		else
+			move = move_input.to_i
+		end
+	move
 	end
 
 	def best_ai_moves(available_moves)
@@ -117,15 +98,34 @@ class NoughtsAndCrosses
 		moves_to_win = 1
 		until moves_to_win > 3
 			possible_dim_arrays.each do |dim_array| #iterates through possible win conditions to find best move. Starts by searching for move that will win in 1.
-				return how_much_win(dim_array).shuffle[0] if how_much_win(dim_array).length == moves_to_win #immediately returns winning move if it finds one.
+				if moves_to_win == 1 && block_player_move
+					puts "AI blocked player!"
+					return block_player_move
+				end
+				if how_much_win(dim_array).length == moves_to_win
+					puts "AI went for a move to win #{moves_to_win} moves."
+					return how_much_win(dim_array).shuffle[0]
+				end
 			end
 		moves_to_win += 1
 		end
 		available_moves.shuffle[0] # returns random move if there are no available moves that could lead to AI victory.
 	end
 
-	def how_much_win(dim_array)
-		dim_array.select {|loc| @loc[loc] != "X"}
+	def how_much_win(dim_array, player="X")
+		dim_array.select {|loc| @loc[loc] != player}
+	end
+
+	def block_player_move #method will decide if player is about to win.
+		puts "AI is trying to understand if player is about to win."
+		move = false
+		possible_dim_arrays = @win_conditions.select {|dim_array| dim_array.select {|loc| @loc[loc] != "X"}.length == 3}
+		possible_dim_arrays.each do |dim_array|
+				if how_much_win(dim_array, "O").length == 1 
+					move = how_much_win(dim_array, "O")[0]
+				end
+			end
+		move
 	end
 
 	def move(move)
